50fcff3611803e499011c798fe4aa5f2df0d24dc
lemongza <92498054+lemongza@users.noreply.github.com>
2025-05-14 01:03:59 +0900

    애플리케이션 직접 생성

--- src/main/java/com/basic/myspringboot/MySpringBootAppApplication.java
+++ src/main/java/com/basic/myspringboot/MySpringBootAppApplication.java
@@ -1,13 +1,21 @@
 package com.basic.myspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MySpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootAppApplication.class, args);
+		//SpringApplication.run(MySpringBootAppApplication.class, args);
+
+		//직접 생성으로
+		SpringApplication application = new SpringApplication(MySpringBootAppApplication.class);
+		//기본적으로 WebApplicationType : 웹어플리케이션 (NONE)
+		// SERVLET으로 변경 (tomcat) -> 계속 살아있음
+		application.setWebApplicationType(WebApplicationType.SERVLET);
+		application.run(args);
 	}
 
 }
