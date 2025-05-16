2aba0c813ad9d4a57d4eaa208ba72c2ebb3e3bda
lemongza <92498054+lemongza@users.noreply.github.com>
2025-05-16 14:33:18 +0900

    get mapping

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -3,10 +3,11 @@ package com.basic.myspringboot.controller;
 import com.basic.myspringboot.entity.User;
 import com.basic.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor //final 변수 자동 초기화 -> 생성자 자동 생성
@@ -25,6 +26,28 @@ public class UserRestController {
     public User create(@RequestBody User user) {
         return userRepository.save(user);
     }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        ResponseEntity<User> responseEntity = optionalUser
+                .map(user -> ResponseEntity.ok(user))
+                .orElse(ResponseEntity.notFound().build());
+
+        return responseEntity;
+
+//        return optionalUser.map(ResponseEntity::ok)
+//                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+
 
 
 
